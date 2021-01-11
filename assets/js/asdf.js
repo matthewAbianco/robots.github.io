4356cf601a6cf387170c86c921ca4b286aef0606
Matt Bianco <matthewbianco@protonmail.com>
2021-01-11 10:45:11 -0500

    restarting the assignment. currently built the robots fight functions with if and else statements

---
+++ assets/js/asdf.js
@@ -0,0 +1,19 @@
+17 lines (11 sloc)  421 Bytes
+  
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
